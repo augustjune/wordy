75d618c01f834110e0972b94545c62fd02c3ba71
Yura Slinkin <jurij.jurich@gmail.com>
2019-02-04 08:45:09 +0100

    Implemented bot for finding synonyms using thesaurus-api

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "0.1"
 scalaVersion := "2.12.8"
 
 libraryDependencies += "com.bot4s" %% "telegram-core" % "4.0.0-RC1"
+
+libraryDependencies += "org.slinkin" %% "thesaurus-api" % "0.1-SNAPSHOT"
--- src/main/scala/WordBot.scala
+++ src/main/scala/WordBot.scala
@@ -1,8 +1,16 @@
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.util.Timeout
+
+import concurrent.duration._
 import com.bot4s.telegram.api.declarative.{Commands, Messages}
 import com.bot4s.telegram.api.{Polling, RequestHandler, TelegramBot}
 import com.bot4s.telegram.clients.SttpClient
 import com.softwaremill.sttp.okhttp.OkHttpFutureBackend
 import slogging.{LogLevel, LoggerConfig, PrintLoggerFactory}
+import thesaurus._
+
+import scala.concurrent.Future
 
 class WordBot(token: String) extends TelegramBot with Messages with Commands with Polling {
   LoggerConfig.factory = PrintLoggerFactory()
@@ -12,7 +20,28 @@ class WordBot(token: String) extends TelegramBot with Messages with Commands wit
   implicit val backend = OkHttpFutureBackend()
   val client: RequestHandler = new SttpClient(token)
 
-  onMessage { msg =>
-    reply(msg.text.getOrElse("You sent me no words :("))(msg)
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+
+  val thesaurus = system.actorOf(ThesaurusApi.props)
+  implicit val timeout = Timeout(3 seconds)
+
+  import akka.pattern.ask
+
+  onCommand("/syn") { msg =>
+    val request = msg.text.flatMap{ s =>
+      val spaceIndex = s.indexOf(" ")
+      if (spaceIndex > 0) Some(s.substring(spaceIndex + 1))
+      else None
+    }
+
+    val responce = request.map { s =>
+      (thesaurus ? WordLookup(s)).mapTo[Either[String, ThesaurusWord]].map(_.map(word => word.synonyms.mkString(", ")))
+    }.getOrElse(Future.successful(Left("No word provided")))
+
+    responce.foreach {
+      case Left(x) => reply(s"An error occurred: $x")(msg)
+      case Right(x) => reply(s"Synonyms: $x")(msg)
+    }
   }
 }
