220a5b6e032f18e8c19823b12befc772ce25cad0
Yura Slinkin <jurij.jurich@gmail.com>
2019-02-08 21:02:51 +0100

    Added different options from thesaurus (antonims, examples)

--- src/main/scala/WordBot.scala
+++ src/main/scala/WordBot.scala
@@ -15,7 +15,7 @@ import scala.concurrent.Future
 class WordBot(token: String) extends TelegramBot with Messages with Commands with Polling {
   LoggerConfig.factory = PrintLoggerFactory()
   // set log level, e.g. to TRACE
-  LoggerConfig.level = LogLevel.TRACE
+  LoggerConfig.level = LogLevel.INFO
 
   implicit val backend = OkHttpFutureBackend()
   val client: RequestHandler = new SttpClient(token)
@@ -35,13 +35,47 @@ class WordBot(token: String) extends TelegramBot with Messages with Commands wit
       else None
     }
 
-    val responce = request.map { s =>
-      (thesaurus ? WordLookup(s)).mapTo[Either[String, ThesaurusWord]].map(_.map(word => word.synonyms.mkString(", ")))
+    val response = request.map { s =>
+      (thesaurus ? WordLookup(s)).mapTo[Either[String, ThesaurusWord]].map(_.map(word => word.synonyms.take(5).mkString(", ")))
     }.getOrElse(Future.successful(Left("No word provided")))
 
-    responce.foreach {
-      case Left(x) => reply(s"An error occurred: $x")(msg)
+    response.foreach {
+      case Left(x) => reply(s"Sorry: $x")(msg)
       case Right(x) => reply(s"Synonyms: $x")(msg)
+    }
+  }
+
+  onCommand("/ant") { msg =>
+    val request = msg.text.flatMap{ s =>
+      val spaceIndex = s.indexOf(" ")
+      if (spaceIndex > 0) Some(s.substring(spaceIndex + 1))
+      else None
+    }
+
+    val response = request.map { s =>
+      (thesaurus ? WordLookup(s)).mapTo[Either[String, ThesaurusWord]].map(_.map(word => word.antonyms.take(5).mkString(", ")))
+    }.getOrElse(Future.successful(Left("No word provided")))
+
+    response.foreach {
+      case Left(x) => reply(s"Sorry: $x")(msg)
+      case Right(x) => reply(s"Antonyms: $x")(msg)
+    }
+  }
+
+  onCommand("/examples") { msg =>
+    val request = msg.text.flatMap{ s =>
+      val spaceIndex = s.indexOf(" ")
+      if (spaceIndex > 0) Some(s.substring(spaceIndex + 1))
+      else None
+    }
+
+    val response = request.map { s =>
+      (thesaurus ? WordLookup(s)).mapTo[Either[String, ThesaurusWord]].map(_.map(word => word.usageExamples.take(3).mkString("\n")))
+    }.getOrElse(Future.successful(Left("No word provided")))
+
+    response.foreach {
+      case Left(x) => reply(s"Sorry: $x")(msg)
+      case Right(x) => reply(s"Examples: \n$x")(msg)
     }
   }
 }
